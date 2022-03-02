da048178ce11ef873077bb313c7029bfa5d969e0
SaberLH <331343LXh>
2022-03-03 00:28:40 +0800

    完善JwtUtil：验证token 根据刷新token获取token

--- src/main/java/com/spring/security/learn/util/JwtUtil.java
+++ src/main/java/com/spring/security/learn/util/JwtUtil.java
@@ -1,16 +1,19 @@
 package com.spring.security.learn.util;
 
 import com.spring.security.learn.config.AppProperties;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -30,6 +33,20 @@ public class JwtUtil {
     public static final Key refreshKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     private final AppProperties appProperties;
+
+    /**
+     * 创建签名 Access Token
+     */
+    public String createAccessToken(UserDetails userDetails) {
+        return createJWTToken(userDetails, appProperties.getJwt().getAccessTokenExpireTime(), key);
+    }
+
+    /**
+     * 创建签名 Refresh Token
+     */
+    public String createRefreshToken(UserDetails userDetails) {
+        return createJWTToken(userDetails, appProperties.getJwt().getRefreshTokenExpireTime(), refreshKey);
+    }
 
     /**
      * 根据用户信息生成一个 JWT
@@ -53,12 +70,60 @@ public class JwtUtil {
                 .signWith(signKey, SignatureAlgorithm.HS512).compact();
     }
 
-    public String createAccessToken(UserDetails userDetails) {
-        return createJWTToken(userDetails, appProperties.getJwt().getAccessTokenExpireTime(), key);
+    /**
+     * 验证签名 Access Token
+     */
+    public boolean validateAccessToken(String jwtToken) {
+        return validateToken(jwtToken, key);
     }
 
-    public String createRefreshToken(UserDetails userDetails) {
-        return createJWTToken(userDetails, appProperties.getJwt().getRefreshTokenExpireTime(), refreshKey);
+    /**
+     * 验证签名 Refresh Token
+     */
+    public boolean validateRefreshToken(String jwtToken) {
+        return validateToken(jwtToken, refreshKey);
+    }
+
+    /**
+     * 验证token
+     * (token过期不包含在此内)
+     * @param jwtToken  token
+     * @param signKey   签名使用的 key
+     * @return token是否合法
+     */
+    public boolean validateToken(String jwtToken, Key signKey) {
+        try {
+            Jwts.parserBuilder().setSigningKey(signKey).build().parseClaimsJws(jwtToken);
+            return true;
+        } catch (ExpiredJwtException | SignatureException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    /**
+     * 根据 Refresh Token 创建 Access Token
+     *
+     * @param jwtToken  Refresh Token
+     * @return Access Token
+     */
+    public String buildAccessTokenWithRefreshToken(String jwtToken) {
+        return parseClaims(jwtToken, refreshKey)
+                .map(claims -> Jwts.builder()
+                        .setClaims(claims)
+                        .setExpiration(new Date(System.currentTimeMillis() + appProperties.getJwt().getAccessTokenExpireTime()))
+                        .setIssuedAt(new Date())
+                        .signWith(key, SignatureAlgorithm.HS512)
+                        .compact())
+                .orElseThrow( () -> new AccessDeniedException("访问被拒绝") );
+    }
+
+    public Optional<Claims> parseClaims(String jwtToken, Key signKey) {
+        try {
+            val claims = Jwts.parserBuilder().setSigningKey(signKey).build().parseClaimsJws(jwtToken).getBody();
+            return Optional.of(claims);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 
 }
